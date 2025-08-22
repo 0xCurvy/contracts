369e796a8791b2303947aa6eed87cb768166ee35
bojinovic <bojinovicc@hotmail.com>
2025-08-22 12:22:57 +0200

    contracts: multicall3 - init

---
+++ script/Localnet_Multicall3_Deploy.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+
+import {Multicall3} from "../src/utils/Multicall3.sol";
+
+contract Localnet_Multicall3_Deploy_Script is Script {
+    function run() public {
+        deployerPk = vm.envUint("DEPLOYER_PK");
+        feeCollector = operator = vm.envAddress("OPERATOR_ADDR");
+
+        vm.startBroadcast(deployerPk);
+
+        multicall3 = new Multicall3();
+        console2.log("Multicall3 deployed at: ", address(multicall3));
+
+        string memory outputFile =
+            string(abi.encodePacked("./deployments/", vm.envString("CHAIN_NAME"), "/Multicall3.address"));
+        vm.writeFile(outputFile, vm.toString(address(multicall3)));
+
+        vm.stopBroadcast();
+    }
+
+    uint256 private deployerPk;
+    address private operator;
+    address private feeCollector;
+
+    Multicall3 public multicall3;
+}
