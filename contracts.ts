321cf0efc347694d541721071d304083f44bb368
Aleksandar Veljkovic <aleksandar.veljkovic.dev@gmail.com>
2025-09-25 11:45:58 +0200

    create wallet client

--- contracts.ts
+++ contracts.ts
@@ -1,6 +1,7 @@
-import { Address, createPublicClient, defineChain, getContract, http } from "viem";
+import { Address, createWalletClient, defineChain, getContract, http } from "viem";
 import CurvyAggregatorArtifacts from './artifacts/contracts/aggregator/CurvyAggregator.sol/CurvyAggregator.json';
 import deployedAddresses from './ignition/deployments/chain-31337/deployed_addresses.json';
+import { privateKeyToAccount } from "viem/accounts"; // Convert private key to account
 
 export const getContracts = (rpcUrl: string = 'http://localhost:8545', networkName: string = 'localhost') => {
 
@@ -19,7 +20,10 @@ export const getContracts = (rpcUrl: string = 'http://localhost:8545', networkNa
         },
     });
 
-    const publicClient = createPublicClient({
+    const account = privateKeyToAccount("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
+
+    const publicClient = createWalletClient({
+        account,
         chain,
         transport: http(rpcUrl),
     });
