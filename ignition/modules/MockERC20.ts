c4c52a5c278e8d4919acfb2cb70147dfae54783a
Aleksandar Veljkovic <aleksandar.veljkovic.dev@gmail.com>
2025-09-26 10:23:57 +0200

    the return of the MockERC20

---
+++ ignition/modules/MockERC20.ts
@@ -0,0 +1,10 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+export default buildModule("ERC20Mock", (m) => {
+  const erc20Mock = m.contract("ERC20Mock");
+
+  // Account #0 in hardhat node
+  m.call(erc20Mock, "mockMint", ["0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", "1000000"]);
+
+  return { erc20Mock };
+});
