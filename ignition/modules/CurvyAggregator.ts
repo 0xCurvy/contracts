fa62aff4a4901c1b8b6a48bb17887ef8398dce71
radojevicMihailo <radojevic.mihailo.00@gmail.com>
2025-10-14 15:14:16 +0200

    wip: testing aggregation passed

--- ignition/modules/CurvyAggregator.ts
+++ ignition/modules/CurvyAggregator.ts
@@ -21,7 +21,7 @@ export default buildModule("CurvyAggregator", (m) => {
       insertionVerifier: curvyInsertionVerifier,
       aggregationVerifier: curvyAggregationVerifier,
       withdrawVerifier: curvyWithdrawVerifier,
-      operator: "0x0000000000000000000000000000000000000000", // TODO use or delete
+      operator: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", // TODO use or delete
       feeCollector: "0x0000000000000000000000000000000000000000", // TODO use or delete
     },
   ]);
