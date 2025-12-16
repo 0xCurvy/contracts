cba6646e542405d9705086ede8f55f5ce19c52c5
Lazar Travica <lazar.travica@route3.co>
2025-12-16 11:46:49 +0100

    updated address reading for PortalFactoryAggregator.ts

--- ignition/modules/PortalFactoryAggregator.ts
+++ ignition/modules/PortalFactoryAggregator.ts
@@ -1,19 +1,16 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import curvyAggregatorAlphaModule from "./CurvyAggregatorAlpha";
 
 export default buildModule("PortalFactoryAggregatorModule", (m) => {
   const owner = m.getAccount(0);
 
-  const curvyVaultProxyAddress = "0xB4BA872fBa00Bc4268067D5DE4223240cEc4B6d5";
-  const curvyAggregatorAlphaProxyAddress = "0x9c07E1Ff4f1B96ae609331BAc327FcC2d8563224";
+  const { curvyVault, curvyAggregatorAlpha } = m.useModule(curvyAggregatorAlphaModule);
+
   const lifiDiamondAddress = "0x0000000000000000000000000000000000000000";
 
   const portalFactory = m.contract("PortalFactory", [owner], { id: "PortalFactory" });
 
-  m.call(portalFactory, "initializeConfig", [
-    curvyVaultProxyAddress,
-    curvyAggregatorAlphaProxyAddress,
-    lifiDiamondAddress,
-  ]);
+  m.call(portalFactory, "initializeConfig", [curvyVault, curvyAggregatorAlpha, lifiDiamondAddress]);
 
   return { portalFactory };
 });
