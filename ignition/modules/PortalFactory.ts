667691ca1c4a9f2b9c2326193839395a48fefa24
Vanja Obradovic <vanja.obradovic99@gmail.com>
2026-01-14 14:59:22 +0100

    feat: new networks

--- ignition/modules/PortalFactory.ts
+++ ignition/modules/PortalFactory.ts
@@ -3,7 +3,8 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("PortalFactoryModule", (m) => {
   const owner = m.getAccount(0);
 
-  // Valid for Ethereum, Optimism, Arbitrum, Polygon, Avalanche
+  // Ethereum, Optimism, Arbitrum, Polygon, Gnosis, Base, BSC - 0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE
+  // Linea - 0xde1e598b81620773454588b85d6b5d4eec32573e
   // Check for other networks at https://docs.li.fi/introduction/lifi-architecture/smart-contract-addresses
   const curvyAggregatorAlphaProxyAddress = "0x0000000000000000000000000000000000000000";
   const curvyVaultProxyAddress = "0x0000000000000000000000000000000000000000";
