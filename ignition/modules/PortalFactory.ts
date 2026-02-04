590ebe2c3855ab45fdedf2364d9e667fef6ccfb9
Lazar Travica <lazar.travica@route3.co>
2026-02-04 21:20:01 +0100

    deployment working

--- ignition/modules/PortalFactory.ts
+++ ignition/modules/PortalFactory.ts
@@ -1,15 +1,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { encodeDeployData, getContractAddress } from "viem";
+import { encodeDeployData, keccak256 } from "viem";
 import artifact from "../../artifacts/contracts/portal/PortalFactory.sol/PortalFactory.json";
 import { getEnvironmentParameter, getNetworkParameter } from "./utils/deployment";
 
-export default buildModule("PortalFactoryModule", (m) => {
-  const ownerAddress = getNetworkParameter("owner");
+export default buildModule("PortalFactory", (m) => {
+  const ownerAddress = getNetworkParameter<`0x{string}`>("owner");
 
   const CREATEX_ADDRESS = "0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed";
   const createX = m.contractAt("ICreateX", CREATEX_ADDRESS, { id: "CreateX" });
-
-  const salt = "0x7374616765206d696861696c6f2c76616e6a6120637572767920706f77657200";
 
   const initCode = encodeDeployData({
     abi: artifact.abi,
@@ -17,22 +15,22 @@ export default buildModule("PortalFactoryModule", (m) => {
     args: [ownerAddress as `0x${string}`],
   });
 
-  const predictedAddress = getContractAddress({
-    from: CREATEX_ADDRESS as `0x${string}`,
-    salt: salt as `0x${string}`,
-    bytecode: initCode,
-    opcode: "CREATE2",
-  });
-
   const create2Salt = getEnvironmentParameter<`0x{string}`>("create2_salt");
   if (!create2Salt) {
     throw new Error("Missing create2_salt environment variable");
   }
+
+  const initCodeHash = keccak256(initCode);
+
   const deployCall = m.call(createX, "deployCreate2(bytes32,bytes)", [create2Salt, initCode], {
     id: "CreateX_PortalFactory_Deploy",
   });
 
-  const portalFactory = m.contractAt("PortalFactory", predictedAddress, {
+  const deployedAddress = m.readEventArgument(deployCall, "ContractCreation(address,bytes32)", "newContract", {
+    emitter: createX,
+  });
+
+  const portalFactory = m.contractAt("PortalFactory", deployedAddress, {
     after: [deployCall],
   });
 
