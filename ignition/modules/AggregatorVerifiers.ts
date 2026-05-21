d1c75af337dc1a3fb39d7af986c2ce5076f06960
radojevicMihailo <radojevic.mihailo.00@gmail.com>
2026-05-21 14:01:42 +0200

    deployment artifacts for staging

---
+++ ignition/modules/AggregatorVerifiers.ts
@@ -0,0 +1,26 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import MainDeployment from "./MainDeployment";
+
+export default buildModule("AggregatorVerifiers", (m) => {
+  const { aggregatorProxy } = m.useModule(MainDeployment);
+
+  const aggregationVerifier = m.contract("CurvyAggregationVerifierAlpha");
+
+  m.call(aggregatorProxy, "updateConfig", [
+    {
+      insertionVerifier: "0x0000000000000000000000000000000000000000",
+      aggregationVerifier,
+      withdrawVerifier: "0x0000000000000000000000000000000000000000",
+      curvyVault: "0x0000000000000000000000000000000000000000",
+      portalFactory: "0x0000000000000000000000000000000000000000",
+      maxDeposits: 0,
+      maxAggregations: 0,
+      maxWithdrawals: 0,
+    },
+  ]);
+
+  return {
+    aggregatorProxy,
+    aggregationVerifier,
+  };
+});
